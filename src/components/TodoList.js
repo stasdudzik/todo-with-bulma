8afcdaf72ab4f0a1fb4241228037cadd7cdf25e1
Stas Dudzik <stas.dudzik@gmail.com>
2020-11-30 15:01:37 +0100

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+/*
+Todo Model Viever Controller
+1. add Todo
+2. display todos
+3. cross off todo
+4.show number of active todos
+5.filter all/active/complete
+6.delete todo
+7.delete all complete
+a) only show if atleast one is complete
+8 button to toggl all on/off
+*/
+
+class TodoList extends React.Component {
+  render() {
+    return <div>Hi from TODO</div>;
+  }
+}
+
+export default TodoList;
